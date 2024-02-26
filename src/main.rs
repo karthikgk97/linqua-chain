722f4759f2b0b7b1f3d53588a4f9d8c553f3236a
Karthik Ganesan <karthik.gk97@gmail.com>
2024-02-25 21:33:27 -0600

    uncommented code

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,26 @@ async fn main(){
     Builder::new().filter_level(log::LevelFilter::Info).init(); 
     log::info!("Main Function");
 
-//    let emb_struct = FastEmbedStruct::new(String::from("BGESmallENV15")).unwrap();
-//
-//    let documents = vec![
-//        "passage: Hello, World!",
-//         "query: Hello, World!",
-//         "passage: This is an example passage."
-//    ];
-//    
-//    log::info!("Model Size {:?}", emb_struct.embedding_config.embedding_model_dimension);
-//    match &emb_struct.embedding_config.embedding_model_object {
-//        EmbeddingModelObject::FastEmbed(embedding_model) => {
-//            if let Ok(embeddings) = embedding_model.embed(documents, None){
-//                log::info!("Embeddings length: {}", embeddings.len());
-//                log::info!("Dimension is {}", embeddings[0].len());
-//            } else {
-//                log::error!("Failed to Embed");
-//            }
-//        },
-//    }
-//
+    let emb_struct = FastEmbedStruct::new(String::from("BGESmallENV15")).unwrap();
+
+    let documents = vec![
+        "passage: Hello, World!",
+         "query: Hello, World!",
+         "passage: This is an example passage."
+    ];
+    
+    log::info!("Model Size {:?}", emb_struct.embedding_config.embedding_model_dimension);
+    match &emb_struct.embedding_config.embedding_model_object {
+        EmbeddingModelObject::FastEmbed(embedding_model) => {
+            if let Ok(embeddings) = embedding_model.embed(documents, None){
+                log::info!("Embeddings length: {}", embeddings.len());
+                log::info!("Dimension is {}", embeddings[0].len());
+            } else {
+                log::error!("Failed to Embed");
+            }
+        },
+    }
+
     let openai_config = OpenAILLMConfig::get_llm_config(OpenAILLMModels::Gpt35_4k, 0.7, 0.95, 512);
 
     let mut chat_history = Vec::new();
